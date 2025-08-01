413fc3fd0b350c9226b38343ab5910a52025ea95
Ayneer Gonzalez <aylgonza@bancolombia.com.co>
2025-08-01 16:51:15 -0500

    fix pay loan dto

--- src/config/dtos/loan/pay_loan_dto.ts
+++ src/config/dtos/loan/pay_loan_dto.ts
@@ -3,10 +3,7 @@ import { datePattern, getParamsErrorMessages } from "../..";
 import { Dto } from "../dto";
 import { PayLoanDtoModel } from "../../../models";
 import { DtoUtil } from "../utils/dto_util";
-import {
-  LoanMovementType,
-  LoanMovementChannel,
-} from "../../../domain/enum";
+import { LoanMovementType, LoanMovementChannel } from "../../../domain/enum";
 
 export class PayLoanDto extends Dto<PayLoanDtoModel> {
   constructor() {
@@ -23,18 +20,26 @@ export class PayLoanDto extends Dto<PayLoanDtoModel> {
       amount: Joi.number()
         .required()
         .messages(getParamsErrorMessages("loan_amount")),
-      type: Joi.valid(...Object.values(LoanMovementType))
+      payments: Joi.array()
+        .items(
+          Joi.object({
+            type: Joi.valid(...Object.values(LoanMovementType))
+              .required()
+              .messages(getParamsErrorMessages("loan_movement_type")),
+            channel: Joi.valid(...Object.values(LoanMovementChannel))
+              .required()
+              .messages(getParamsErrorMessages("loan_movement_channel")),
+            date: Joi.string()
+              .regex(datePattern)
+              .messages(getParamsErrorMessages("loan_initial_date")),
+            description: Joi.string().messages(
+              getParamsErrorMessages("loan_description")
+            ),
+          })
+        )
+        .min(1)
         .required()
-        .messages(getParamsErrorMessages("loan_movement_type")),
-      channel: Joi.valid(...Object.values(LoanMovementChannel))
-        .required()
-        .messages(getParamsErrorMessages("loan_movement_channel")),
-      date: Joi.string()
-        .regex(datePattern)
-        .messages(getParamsErrorMessages("loan_initial_date")),
-      description: Joi.string().messages(
-        getParamsErrorMessages("loan_description")
-      ),
+        .messages(getParamsErrorMessages("loan_payments")),
     })
       .unknown(true)
       .messages(getParamsErrorMessages("loan_object"));
@@ -48,18 +53,14 @@ export class PayLoanDto extends Dto<PayLoanDtoModel> {
         undefined,
       ];
     } else {
-      const { loanId, amount, date, user, type, channel, description } = object;
+      const { loanId, ownerId, payments } = object;
       return [
         undefined,
         undefined,
         new PayLoanDtoModel({
           loanId,
-          amount,
-          date: date ?? null,
-          type: type,
-          channel: channel,
-          description: description,
-          ownerId: user.id,
+          ownerId,
+          payments,
         }),
       ];
     }
